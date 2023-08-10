245c0c00c9c917daca87ad3ead2fe4f6a9206122
citrus327 <citrus327@outlook.com>
2023-08-10 17:22:08 +0800

    feat: press q to terminate program

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io::{self, Read};
 
 fn main() {
+    println!("Hecto is running!");
     for b in io::stdin().bytes() {
-        let c = b.unwrap();
+        let c = b.unwrap() as char;
         println!("{}", c);
+
+        if c == 'q' {
+            break;
+        }
     }
 }
