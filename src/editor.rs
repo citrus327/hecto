58fbea527266dd462c9c560a92a75b0ed1e4efe6
citrus327 <citrus327@outlook.com>
2023-08-12 15:14:56 +0800

    feat: remove printings

--- src/editor.rs
+++ src/editor.rs
@@ -7,36 +7,41 @@ fn die(e: &std::io::Error) {
     panic!("{}", &e);
 }
 
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+
 impl Editor {
     pub fn run(&self) {
         // 修改terminal的cooked mode为raw mode，即输入后不需要按回车响应用户输入
         // termion会修改老的stdout, 使用_stdout来hold之前老的，否则新的termion会属于unowned, 则会被移除，直接进入cooked mode
         let _stdout = io::stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        /*
-                         * 判断是否为控制按键输入
-                         * Control characters are non-printable characters that we don’t want to print to the screen.
-                         * ASCII codes 0–31 are all control characters, and 127 is also a control character.
-                         * ASCII codes 32–126 are all printable
-                         */
-                        if c.is_control() {
-                            println!("{c:?}\r");
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{key:?}\r"),
-                },
-                Err(e) => die(&e),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(&error);
             }
+            // match self.process_keypress() {
+            //     Ok(key) => println!("{:?}\r", key),
+            //     Err(error) => die(&error),
+            // }
         }
     }
     pub fn default() -> Self {
         Self {}
+    }
+
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program Exited!"),
+            _ => (),
+        };
+
+        Ok(())
     }
 }
