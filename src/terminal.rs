6be4a80b49c1e63176bbee9a3de988aab4769f40
citrus327 <citrus327@outlook.com>
2023-08-12 17:17:18 +0800

    feat: terminal

---
+++ src/terminal.rs
@@ -0,0 +1,14 @@
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+pub struct Terminal {
+    size: Size,
+}
+
+impl Terminal {
+  pub fn default() {
+    let size = termion::ter
+  }
+}
