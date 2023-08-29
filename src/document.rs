7227ac48a51b40d7634d7b799352585764cfe833
citrus327 <citrus327@outlook.com>
2023-08-29 11:26:35 +0800

    feat: insertion

--- src/document.rs
+++ src/document.rs
@@ -1,6 +1,6 @@
+use crate::Position;
+use crate::Row;
 use std::fs;
-
-use crate::{editor::Position, row::Row};
 
 #[derive(Default)]
 pub struct Document {
@@ -9,40 +9,34 @@ pub struct Document {
 }
 
 impl Document {
-    pub fn open(file_name: &str) -> Result<Self, std::io::Error> {
-        let contents = fs::read_to_string(file_name)?;
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
+        let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
-
         for value in contents.lines() {
-            rows.push(Row::from(value))
+            rows.push(Row::from(value));
         }
-
         Ok(Self {
-            rows: rows,
-            file_name: Some(file_name.to_string()),
+            rows,
+            file_name: Some(filename.to_string()),
         })
     }
-
-    pub fn get_row(&self, index: usize) -> Option<&Row> {
+    pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
     }
-
     pub fn is_empty(&self) -> bool {
         self.rows.is_empty()
     }
-
     pub fn len(&self) -> usize {
         self.rows.len()
     }
-
     pub fn insert(&mut self, at: &Position, c: char) {
         if at.y == self.len() {
             let mut row = Row::default();
-            row.insert(at.x, c);
+            row.insert(0, c);
             self.rows.push(row);
-        } else {
+        } else if at.y < self.len() {
             let row = self.rows.get_mut(at.y).unwrap();
-            row.insert(at.x, c)
+            row.insert(at.x, c);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+#![warn(clippy::all, clippy::pedantic)]
 mod document;
+mod editor;
 mod row;
 mod terminal;
-use editor::Editor;
-// 将terminal下的Terminal暴露至crate scope
 pub use document::Document;
+use editor::Editor;
+pub use editor::Position;
+pub use row::Row;
 pub use terminal::Terminal;
 
-mod editor;
-
 fn main() {
-    Editor::default().run()
+    Editor::default().run();
 }
